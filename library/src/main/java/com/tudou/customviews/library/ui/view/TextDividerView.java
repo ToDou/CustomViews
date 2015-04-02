ebc899af20ce5e66936c0f3f8c389b78c0842c02
ToDou <yytodou123@gmail.com>
2015-04-02 23:02:05 +0800

    add the styleable

--- library/src/main/java/com/tudou/customviews/library/ui/view/TextDividerView.java
+++ library/src/main/java/com/tudou/customviews/library/ui/view/TextDividerView.java
@@ -1,112 +1,125 @@
 package com.tudou.customviews.library.ui.view;
 
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.graphics.RectF;
 import android.util.AttributeSet;
 import android.view.View;
 
+import com.tudou.customviews.library.R;
+
 /**
  * Created by tudou on 15-4-1.
  */
 public class TextDividerView extends View {
 
-  private Paint mTextPaint;
-  private Paint mDividerPaint;
-  private RectF mDividerRectFPre;
-  private RectF mDividerRectFAfter;
+    private Paint mTextPaint;
+    private Paint mDividerPaint;
+    private RectF mDividerRectFPre;
+    private RectF mDividerRectFAfter;
 
-  private int mTextColor;
-  private int mDividerColor;
-  private int mColor;
+    private int mTextColor;
+    private int mDividerColor;
+    private int mColor;
 
-  private float mTextSize;
-  private float mRectHeight;
+    private float mTextSize;
+    private float mRectHeight;
 
-  private float mTextWidth;
-  private float mTextBaseX;
-  private float mTextBaseY;
-  private float mTextOffset;
+    private float mTextWidth;
+    private float mTextBaseX;
+    private float mTextBaseY;
+    private float mTextOffset;
 
-  private String mFix = " ";
-  private String mText = "or";
+    private String mFix = " ";
+    private String mText = "or";
 
-  public TextDividerView(Context context) {
-    this(context, null);
-  }
+    public TextDividerView(Context context) {
+        this(context, null);
+    }
 
-  public TextDividerView(Context context, AttributeSet attrs) {
-    this(context, attrs, 0);
-  }
+    public TextDividerView(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
 
-  public TextDividerView(Context context, AttributeSet attrs, int defStyleAttr) {
-    super(context, attrs, defStyleAttr);
+    public TextDividerView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
 
-    mTextColor = getResources().getColor(android.R.color.darker_gray);
-    mDividerColor = getResources().getColor(android.R.color.darker_gray);
-    mColor = mTextColor;
-    mTextSize = sp2px(16);
-    mRectHeight = dp2px(1.0f);
-    mTextOffset = dp2px(5.0f);
+        mTextColor = getResources().getColor(android.R.color.darker_gray);
+        mDividerColor = getResources().getColor(android.R.color.darker_gray);
+        mColor = mTextColor;
+        mTextSize = sp2px(16);
+        mRectHeight = dp2px(1.0f);
+        mTextOffset = dp2px(5.0f);
 
-    initPainters();
-    initRectFs();
-  }
+        TypedArray array = context.obtainStyledAttributes(attrs, R.styleable.TextDividerView);
+        mTextColor = array.getColor(R.styleable.TextDividerView_text_color, mTextColor);
+        mDividerColor = array.getColor(R.styleable.TextDividerView_line_color, mDividerColor);
+        mColor = mTextColor;
+        mTextSize = array.getDimension(R.styleable.TextDividerView_text_size, mTextSize);
+        mRectHeight = array.getDimension(R.styleable.TextDividerView_line_height, mRectHeight);
+        mTextOffset = array.getDimension(R.styleable.TextDividerView_text_offset, mTextOffset);
 
-  private void initRectFs() {
-    mDividerRectFAfter = new RectF(0, 0, 0, 0);
-    mDividerRectFPre = new RectF(0, 0, 0, 0);
-  }
+        array.recycle();
 
-  private void initPainters() {
-    mTextPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
-    mTextPaint.setTextSize(mTextSize);
-    mTextPaint.setColor(mTextColor);
+        initPainters();
+        initRectFs();
+    }
 
-    mDividerPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
-    mDividerPaint.setColor(mDividerColor);
-  }
+    private void initRectFs() {
+        mDividerRectFAfter = new RectF(0, 0, 0, 0);
+        mDividerRectFPre = new RectF(0, 0, 0, 0);
+    }
 
-  @Override
-  protected void onDraw(Canvas canvas) {
-    super.onDraw(canvas);
-    calculateRectFs();
+    private void initPainters() {
+        mTextPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        mTextPaint.setTextSize(mTextSize);
+        mTextPaint.setColor(mTextColor);
 
-    canvas.drawRect(mDividerRectFPre, mDividerPaint);
-    canvas.drawRect(mDividerRectFAfter, mDividerPaint);
+        mDividerPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        mDividerPaint.setColor(mDividerColor);
+    }
 
-    canvas.drawText(mText, mTextBaseX, mTextBaseY, mTextPaint);
-  }
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        calculateRectFs();
 
-  private void calculateRectFs() {
-    mText = mFix + mText + mFix;
-    mTextWidth = mTextPaint.measureText(mText);
+        canvas.drawRect(mDividerRectFPre, mDividerPaint);
+        canvas.drawRect(mDividerRectFAfter, mDividerPaint);
 
-    mTextBaseY =
-        (int) ((getHeight() / 2.0f) - ((mTextPaint.descent() + mTextPaint.ascent()) / 2.0f));
+        canvas.drawText(mText, mTextBaseX, mTextBaseY, mTextPaint);
+    }
 
-    mDividerRectFPre.left = getPaddingLeft();
-    mDividerRectFPre.top = getHeight() / 2 - mRectHeight / 2;
-    mDividerRectFPre.right = getWidth() / 2 - mTextWidth / 2 - mTextOffset;
-    mDividerRectFPre.bottom = getHeight() / 2 + mRectHeight / 2;
+    private void calculateRectFs() {
+        mText = mFix + mText + mFix;
+        mTextWidth = mTextPaint.measureText(mText);
 
-    mTextBaseX = mDividerRectFPre.right + mTextOffset;
+        mTextBaseY =
+                (int) ((getHeight() / 2.0f) - ((mTextPaint.descent() + mTextPaint.ascent()) / 2.0f));
 
-    mDividerRectFAfter.left = getWidth() / 2 + mTextWidth / 2 + mTextOffset;
-    mDividerRectFAfter.top = getHeight() / 2 - mRectHeight / 2;
-    mDividerRectFAfter.right = getWidth() - getPaddingRight();
-    mDividerRectFAfter.bottom = getHeight() / 2 + mRectHeight / 2;
-  }
+        mDividerRectFPre.left = getPaddingLeft();
+        mDividerRectFPre.top = getHeight() / 2 - mRectHeight / 2;
+        mDividerRectFPre.right = getWidth() / 2 - mTextWidth / 2 - mTextOffset;
+        mDividerRectFPre.bottom = getHeight() / 2 + mRectHeight / 2;
 
-  public float dp2px(float dp) {
-    final float scale = getResources().getDisplayMetrics().density;
-    return dp * scale + 0.5f;
-  }
+        mTextBaseX = mDividerRectFPre.right + mTextOffset;
 
-  public float sp2px(float sp) {
-    final float scale = getResources().getDisplayMetrics().scaledDensity;
-    return sp * scale;
-  }
+        mDividerRectFAfter.left = getWidth() / 2 + mTextWidth / 2 + mTextOffset;
+        mDividerRectFAfter.top = getHeight() / 2 - mRectHeight / 2;
+        mDividerRectFAfter.right = getWidth() - getPaddingRight();
+        mDividerRectFAfter.bottom = getHeight() / 2 + mRectHeight / 2;
+    }
+
+    public float dp2px(float dp) {
+        final float scale = getResources().getDisplayMetrics().density;
+        return dp * scale + 0.5f;
+    }
+
+    public float sp2px(float sp) {
+        final float scale = getResources().getDisplayMetrics().scaledDensity;
+        return sp * scale;
+    }
 
 }
