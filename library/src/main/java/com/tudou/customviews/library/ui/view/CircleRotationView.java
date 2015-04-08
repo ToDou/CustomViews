97ad2d5c89dc35df4e8deddbc741efe61fd89727
ToDou <yytodou123@gmail.com>
2015-04-08 22:26:09 +0800

    add the circlerotationview

---
+++ library/src/main/java/com/tudou/customviews/library/ui/view/CircleRotationView.java
@@ -0,0 +1,24 @@
+package com.tudou.customviews.library.ui.view;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by tudou on 15-4-8.
+ */
+public class CircleRotationView extends View {
+
+  public CircleRotationView(Context context) {
+    super(context);
+  }
+
+  public CircleRotationView(Context context, AttributeSet attrs) {
+    super(context, attrs);
+  }
+
+  public CircleRotationView(Context context, AttributeSet attrs, int defStyleAttr) {
+    super(context, attrs, defStyleAttr);
+  }
+}
